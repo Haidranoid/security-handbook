a977447a7a60a940419b0cceda0a89c92c87d664
Jose Eduardo Martinez Lara <martinezlara_joseeduardo@hotmail.com>
2024-03-19 18:06:59 -0600

    initial commit

---
+++ src/main/java/com/alibou/security/demo/DemoController.java
@@ -0,0 +1,20 @@
+package com.alibou.security.demo;
+
+import com.alibou.security.user.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/demo-controller")
+public class DemoController {
+
+    @GetMapping
+    public ResponseEntity<String> sayHello(Authentication authentication) {
+        var user = (User) authentication.getPrincipal();
+        var message = "Hello, " + user.getEmail();
+        return ResponseEntity.ok(message);
+    }
+}
